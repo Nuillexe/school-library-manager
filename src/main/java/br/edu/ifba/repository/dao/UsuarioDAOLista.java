ef5c42d6dc49d7d12aa29975e4b805100b1637f6
anaclaramarinhoviana77-alt <anaclaramarinhoviana77@gmail.com>
2026-06-28 18:08:18 -0300

    Refatora DAOs para utilização do Collections Framework

--- src/main/java/br/edu/ifba/repository/dao/UsuarioDAOLista.java
+++ src/main/java/br/edu/ifba/repository/dao/UsuarioDAOLista.java
@@ -1,25 +1,25 @@
 package br.edu.ifba.repository.dao;
 
 import br.edu.ifba.models.Usuario;
-import br.edu.ifba.ed.ListaDinamica;
-import br.edu.ifba.ed.Listavel;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsuarioDAOLista {
 
-    private Listavel<Usuario> listaDeUsuarios = new ListaDinamica<>();
+    private List<Usuario> listaDeUsuarios = new ArrayList<>();
 
     //  Salvar usuário
     public void salvar(Usuario u) {
         if (u == null) {
             throw new IllegalArgumentException("Usuário não pode ser nulo.");
         }
-        listaDeUsuarios.anexar(u);
+        listaDeUsuarios.add(u);
     }
 
     //  Buscar por ID
     public Usuario buscarPorId(String id) {
-        for (int i = 0; i < listaDeUsuarios.tamanho(); i++) {
-            Usuario u = listaDeUsuarios.selecionar(i);
+        for (int i = 0; i < listaDeUsuarios.size(); i++) {
+            Usuario u = listaDeUsuarios.get(i);
             if (u.getId().equals(id)) {
                 return u;
             }
@@ -29,10 +29,10 @@ public class UsuarioDAOLista {
 
     //  Listar todos
     public Usuario[] listar() {
-        Usuario[] arrayRetorno = new Usuario[listaDeUsuarios.tamanho()];
+        Usuario[] arrayRetorno = new Usuario[listaDeUsuarios.size()];
 
-        for (int i = 0; i < listaDeUsuarios.tamanho(); i++) {
-            arrayRetorno[i] = listaDeUsuarios.selecionar(i);
+        for (int i = 0; i < listaDeUsuarios.size(); i++) {
+            arrayRetorno[i] = listaDeUsuarios.get(i);
         }
 
         return arrayRetorno;
@@ -40,11 +40,11 @@ public class UsuarioDAOLista {
 
     //  Atualizar usuário pelo ID
     public void atualizar(String id, Usuario usuarioAtualizado) {
-        for (int i = 0; i < listaDeUsuarios.tamanho(); i++) {
-            Usuario u = listaDeUsuarios.selecionar(i);
+        for (int i = 0; i < listaDeUsuarios.size(); i++) {
+            Usuario u = listaDeUsuarios.get(i);
 
             if (u.getId().equals(id)) {
-                listaDeUsuarios.atualizar(usuarioAtualizado, i);
+                listaDeUsuarios.set(i, usuarioAtualizado);
                 return;
             }
         }
@@ -54,11 +54,11 @@ public class UsuarioDAOLista {
 
     //  Apagar usuário pelo ID
     public Usuario apagar(String id) {
-        for (int i = 0; i < listaDeUsuarios.tamanho(); i++) {
-            Usuario u = listaDeUsuarios.selecionar(i);
+        for (int i = 0; i < listaDeUsuarios.size(); i++) {
+            Usuario u = listaDeUsuarios.get(i);
 
             if (u.getId().equals(id)) {
-                return listaDeUsuarios.apagar(i);
+                return listaDeUsuarios.remove(i);
             }
         }
         return null;
